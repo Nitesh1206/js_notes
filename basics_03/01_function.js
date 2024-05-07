5bc2baa83901bd3e10c37c199c329f2660c472f5
Nitesh Tak <82152289+Nitesh1206@users.noreply.github.com>
2024-05-07 10:08:54 +0000

    functions with array and object

--- basics_03/01_function.js
+++ basics_03/01_function.js
@@ -34,3 +34,43 @@ function greeting(user){
 }
 
 console.log(greeting("nitesh"))
+
+// how to hold infinite value in function parameter
+
+function calculateCartValue(...num){ // rest operator is use to hold multiple value in a functions parameter
+    return num
+}
+console.log(calculateCartValue(50, 20, 10));  // output : [ 50, 20, 10 ]
+
+function cart(val1, val2, ...num){
+    return num
+}
+console.log(cart(20, 40, 90, 10)); // output : [ 90, 10 ]
+
+// note : rest operator create an array of passed parameter 
+
+
+// using of objects and arrays inside a function
+
+const newOrder = {
+    category : "Shoes",
+    price : 999
+}
+
+function order(placeOrder){
+    console.log(`your ${placeOrder.category} are of ${placeOrder.price} rupees`)
+}
+order(newOrder); // instead of reference you can also pass the direct object here
+order({
+    category : "Shoes",
+    price : 999
+}) // we can also pass or declare object directly in function calling without a variable
+
+const cancelOrder = ["shirt", "jacket", "shorts"]
+
+function myPreviousOrder(rejected){
+    return rejected[2];
+}
+
+console.log(myPreviousOrder(cancelOrder)) // instead of reference you can also pass the direct array here
+console.log(myPreviousOrder(["shirt", "jacket", "shorts"])) // you can pass an array without variable also, like this
